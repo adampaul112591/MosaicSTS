b38581bc5114336fe6c9f3fe8b4f3dfc1cd6a3f3
protocols <protocols@nate.com>
2016-07-17 14:28:33 +0900

    first commit

---
+++ src/main/java/com/hybrid/MosaicStsApplication.java
@@ -0,0 +1,12 @@
+package com.hybrid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaicStsApplication.class, args);
+	}
+}
